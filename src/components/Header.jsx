eabb48fcd7f65eb8e51504167691f3094d1507c7
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-04-28 14:40:38 +0200

    Mobile header created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,67 @@
 import React, { useState } from 'react'
 import { HiOutlineFilm } from 'react-icons/hi'
 import { CiSearch } from 'react-icons/ci'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { IoMdClose } from 'react-icons/io'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
   const [showInput, setShowInput] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="md:flex justify-between bg-[#111827]">
+    <div className="flex justify-between bg-[#111827]">
       <div className="p-5 flex">
         <HiOutlineFilm className="text-[#EF4444] text-3xl mx-2" />
         <h1 className="text-xl text-white font-bold">MovieFlix</h1>
       </div>
       <div className="flex p-5">
-        <ul className="text-white mt-0.5">
+        {/* Mobile */}
+        <div className="md:hidden mx-5 mt-2 text-white text-xl ">
+          <button className="cursor-pointer" onClick={() => setIsOpen(true)}>
+            <RxHamburgerMenu />
+          </button>
+          {isOpen && (
+            <div className="bg-black text-white fixed top-19 z-50 left-3 h-100 w-115 transition delay-150 duration-300 ease-in-out">
+              <div className="flex justify-end px-5 py-3 ">
+                <button
+                  onClick={() => setIsOpen(false)}
+                  className="text-white text-xl cursor-pointer"
+                >
+                  <IoMdClose />
+                </button>
+              </div>
+              <ul className="flex flex-col text-center text-white mt-0.5">
+                <Link
+                  className="px-3 py-5 hover:text-[#EF4444] transition delay-150 duration-300 ease-in-out"
+                  to={'/'}
+                >
+                  Home
+                </Link>
+                <Link
+                  className="px-3 py-5 hover:text-[#EF4444] transition delay-150 duration-300 ease-in-out"
+                  to={'/movies'}
+                >
+                  Movies
+                </Link>
+                <Link
+                  className="px-3 py-5 hover:text-[#EF4444] transition delay-150 duration-300 ease-in-out"
+                  to={'/tvshows'}
+                >
+                  TV Shows
+                </Link>
+                <Link
+                  className="pl-3 py-5 pr-5 hover:text-[#EF4444] transition delay-150 duration-300 ease-in-out"
+                  to={'/mylist'}
+                >
+                  My List
+                </Link>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        {/* End Mobile */}
+        <ul className="text-white mt-0.5 max-md:hidden">
           <Link
             className="px-3 hover:text-[#EF4444] transition delay-150 duration-300 ease-in-out"
             to={'/'}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className='bg-[url("https://miro.medium.com/v2/resize:fit:1400/1*xLahrUiFfXAs8s_Q8JKcbA@2x.jpeg")] w-full h-164 bg-cover bg-center'>
+      <div className="grid content-center bg-black/30 h-full w-full">
+        <div className="p-9">
+          <h1 className="text-white text-3xl font-bold">Inception</h1>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
