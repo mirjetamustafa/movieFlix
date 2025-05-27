0b6b3e17152c3f69bdc83ef6cf2d11d099d8501a
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-05-27 14:58:07 +0200

    Updated footer

--- src/components/BackgroundShows.jsx
+++ src/components/BackgroundShows.jsx
@@ -19,11 +19,11 @@ const BackgroundShows = ({ tvShows, getGenreNames, genresTvList }) => {
       >
         <div className="grid content-center bg-black/30 h-full w-full rounded-xl">
           <div className="px-15 pt-35 max-md:text-center">
-            <h1 className="text-white text-4xl font-bold">
+            <h1 className="text-white md:text-4xl  font-bold">
               {shows?.original_name || 'Loading...'}
             </h1>
             <div className="flex max-md:justify-center">
-              <p className="bg-red-600 rounded-sm px-2 py-1 mt-5 text-white text-sm">
+              <p className="bg-red-600 rounded-sm px-2 py-1 mt-5 text-white text-xs md:text-sm">
                 {shows?.adult ? '18+' : 'PG-13'}
               </p>
               <p className="pl-4 pr-2 py-1 mt-5 text-white text-sm">
@@ -33,10 +33,10 @@ const BackgroundShows = ({ tvShows, getGenreNames, genresTvList }) => {
                 {getGenreNames(shows?.genre_ids, genresTvList)}
               </p>
             </div>
-            <p className="text-md text-white mt-3 pr-[50%]">
+            <p className="text-md text-white mt-3 md:pr-[50%]">
               {shows?.overview}
             </p>
-            <div className="flex max-md:justify-center">
+            <div className="flex max-md:justify-center mb-9">
               <button className="bg-red-600 flex rounded-full px-4 py-2 mt-3 text-white hover:bg-red-700 cursor-pointer">
                 <CiPlay1 className="mt-0.5 text-xl mr-1" />
                 Play
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,104 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { LuFacebook } from 'react-icons/lu'
+import { FiTwitter, FiYoutube } from 'react-icons/fi'
+import { FaInstagram } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <div className="bg-[#111827]">
-      <div className="border-b border-gray-500 mx-5" />
+    <div className="bg-[#1e242e]">
       <div className="grid md:grid-cols-4 p-5">
         <div className="m-4">
           <h1 className="text-red-600 text-2xl font-bold">MovieFlix</h1>
-          <p className="text-sm text-gray-400 pr-29 mt-4">
+          <p className="text-sm text-gray-400 md:pr-29 max-md:pr-20 mt-4">
             Your ultimate destination for movies and TV shows. Stream thousands
             of titles anytime, anywhere.
           </p>
         </div>
-        <div className="bg-red-400 h-20 m-4"></div>
-        <div className="bg-red-400 h-20 m-4"></div>
-        <div className="bg-red-400 h-20 m-4"></div>
+        <div className="m-4">
+          <h1 className="text-white text-lg font-semibold">Browse</h1>
+          <ul className="text-gray-400 flex flex-col">
+            <Link to={'/movies'} className="hover:text-gray-300">
+              Movies
+            </Link>
+            <Link to={'/tvshows'} className="hover:text-gray-300">
+              TV Shows
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Documentaries
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Kids
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              New Releases
+            </Link>
+          </ul>
+        </div>
+        <div className="m-4">
+          <h1 className="text-white text-lg font-semibold">Account</h1>
+          <ul className="text-gray-400 flex flex-col">
+            <Link to={'/mylist'} className="hover:text-gray-300">
+              My List
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Account Settings
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Subscription
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Help Center
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Contact Us
+            </Link>
+          </ul>
+        </div>
+        <div className="m-4">
+          <h1 className="text-white text-lg font-semibold">Company</h1>
+          <ul className="text-gray-400 flex flex-col">
+            <Link to={'/'} className="hover:text-gray-300">
+              About Us
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Careers
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Press
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Privacy Policy
+            </Link>
+            <Link to={'/'} className="hover:text-gray-300">
+              Terms of Service
+            </Link>
+          </ul>
+        </div>
+      </div>
+      <div className="border-b border-white/10 mx-5" />
+      <div className="grid md:grid-cols-2 md:justify-between justify-center mt-7">
+        <div className="m-4 ml-15">
+          <ul className="flex">
+            <Link to={'/'}>
+              <LuFacebook className="text-gray-400 text-xl mx- hover:text-gray-300" />
+            </Link>
+            <Link to={'/'}>
+              <FiTwitter className="text-gray-400 text-xl mx-2 hover:text-gray-300" />
+            </Link>
+            <Link to={'/'}>
+              <FaInstagram className="text-gray-400 text-xl mx-2 hover:text-gray-300" />
+            </Link>
+            <Link to={'/'}>
+              <FiYoutube className="text-gray-400 text-xl mx-2 hover:text-gray-300" />
+            </Link>
+          </ul>
+        </div>
+        <div className="m-4">
+          <p className="flex md:justify-end text-sm text-gray-400 mr-5">
+            © 2024 MovieFlx. All rights reserved.
+          </p>
+        </div>
       </div>
     </div>
   )
